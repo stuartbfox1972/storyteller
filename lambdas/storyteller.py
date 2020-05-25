c4d82d414c1209db1374281556dcb60d122c6e22
Stuart Fox <stuart@stuartfox.ca>
2020-05-25 09:03:16 -0700

    2

--- lambdas/storyteller.py
+++ lambdas/storyteller.py
@@ -6,13 +6,16 @@ from boto3.dynamodb.conditions import Key, Attr
 
 def lambda_handler(event, context):
   if event['resource'] == '/story':
+    dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
     table = dynamodb.Table('ag-stories')
     response = table.scan()
     for i in response['Items']:
       print(json.dumps(i))
-
-if __name__ == "__main__":
-  with open('request.test') as f:
-    data = json.load(f)
-  context = '{}'
-  lambda_handler(data, context)
+  elif event['resource'] == '/story/{id}':
+    dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
+    table = dynamodb.Table('ag-stories')
+    print(event['pathParameters']['id'])
+    response = table.get_item(Key={'storyid': event['pathParameters']['id']})
+    print(json.dumps(response['Item']))
+  else:
+      return json.dumps('{"Error":"Unsupported Operation"}')
