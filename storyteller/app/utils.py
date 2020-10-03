efd6a84fc3ba8137a979779b6ae1e0a31a9c0d29
Stuart Fox <stuart@stuartfox.ca>
2020-10-02 18:10:41 -0700

    Add verify=False to token decoding :(

--- storyteller/app/utils.py
+++ storyteller/app/utils.py
@@ -32,19 +32,10 @@ def _get_secret():
             return (secret)
 
 def _decode_token():
-
-    secrets=_get_secret()
-
     jwt_header = request.headers.get('X-Amzn-Oidc-Data')
-    if jwt_header is None:
-        jwt_header = "eyJ0eXAiOiJKV1QiLCJraWQiOiI2OWVmNGNhZS0wYWJmLTRjNTItYWIyNS03NGI2NDA3MGJlMGUiLCJhbGciOiJFUzI1NiIsImlzcyI6Imh0dHBzOi8vY29nbml0by1pZHAuZXUtY2VudHJhbC0xLmFtYXpvbmF3cy5jb20vZXUtY2VudHJhbC0xXzFLaWxSckN0ViIsImNsaWVudCI6InIzdmwxcXE5ZHFuNXJqYTNjdTBvZTZzZ2UiLCJzaWduZXIiOiJhcm46YXdzOmVsYXN0aWNsb2FkYmFsYW5jaW5nOmV1LWNlbnRyYWwtMTo2ODk2ODAwODQwMzU6bG9hZGJhbGFuY2VyL2FwcC9jb2duaS1GYXJnYS0xMkZMQUEwVkRHQ1dXLzgzZDQxNTBkNmFmM2RiOTYiLCJleHAiOjE1ODM1ODY3ODB9.eyJzdWIiOiIzZjFmODc4My04ZTI3LTQ3M2QtODUyZS05MGM5NGM0ZjI3MGIiLCJ1c2VybmFtZSI6Im1ib3JnbWVpZXJAdGVjcmFjZXIuZGUiLCJleHAiOjE1ODM1ODY3ODAsImlzcyI6Imh0dHBzOi8vY29nbml0by1pZHAuZXUtY2VudHJhbC0xLmFtYXpvbmF3cy5jb20vZXUtY2VudHJhbC0xXzFLaWxSckN0ViJ9.nXCE9-LtOfxeRGYia4THH8U4xhKv15Sr3H-lzCLAnJ9p8kJ3kkZie6gfd-Yen3SzonB45Ycu0uSrS5X7JUyo2A"
+    
+    #TODO: Fix this!! Grrr
+    jwt_decoded = jwt.decode(jwt_header, verify=False)
 
-    #TODO: In production you WANT to verify the signature!
-    jwt_decoded = jwt.decode(jwt_header, verify=True)
-
-    variables = {
-        "username": jwt_decoded["username"],
-        "valid_until_utc": datetime.fromtimestamp(jwt_decoded["exp"],tz=pytz.UTC).isoformat(),
-        "jwt_decoded": json.dumps(jwt_decoded, indent=4),
-    }
-    return variables
+    payload = json.dumps(jwt_decoded)
+    return payload
