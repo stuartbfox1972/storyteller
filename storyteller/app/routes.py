02b7bf4110ec9c2a20d697366bb41c07aead5a44
Stuart Fox <stuart@stuartfox.ca>
2020-10-05 15:38:48 -0700

    Still banging my head against jwt sigs v6

--- storyteller/app/routes.py
+++ storyteller/app/routes.py
@@ -1,7 +1,6 @@
 from app import app
-from app.stories import _list_stories
 from app.utils import _decode_token
-from flask import jsonify, make_response, redirect, render_template, request, Response
+from flask import render_template, request, Response
 from aws_xray_sdk.core import patch_all
 
 patch_all()
@@ -19,14 +18,6 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/logout')
-def logout():
-    response = make_response(redirect(os.environ.get("LOGOUT_URL]", f"https://{request.host}/")))
-    response.set_cookie("AWSELBAuthSessionCookie-0", "empty", max_age=-3600)
-
-    return response
-
-
 @app.route('/health')
 def health():
     return Response("OK", mimetype='text/text')
@@ -36,10 +27,4 @@ def health():
 def api():
     payload = _decode_token()
     # #return render_template("debug.html", **variables)
-    return Response(payload, mimetype='application/json')
-
-
-@app.route('/api/v1.0/stories', methods=['GET'])
-def get_stories():
-    payload = _list_stories()
     return Response(payload, mimetype='application/json')
