749a7b9259b2a45903a2e66c1a46f996b633af7f
Stuart Fox <stuart@stuartfox.ca>
2020-10-05 16:15:06 -0700

    Still banging my head against jwt sigs v8

--- storyteller/app/routes.py
+++ storyteller/app/routes.py
@@ -1,6 +1,6 @@
 from app import app
 from app.utils import _decode_token
-from flask import render_template, request, Response
+from flask import render_template, request, Response, send_from_directory
 from aws_xray_sdk.core import patch_all
 
 patch_all()
@@ -11,6 +11,12 @@ def apply_headers(response):
     response.headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
     response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
     return response
+
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 
 @app.route('/', methods=['GET'])
