629a57bdc553d5b0f00d4b310c5d887f0c21c730
Stuart Fox <stuart@stuartfox.ca>
2020-10-14 13:41:12 -0700

    Get the lambda working

--- lambdas/story/story.py
+++ lambdas/story/story.py
@@ -5,15 +5,29 @@ import os
 
 from boto3.dynamodb.conditions import Key, Attr
 
-def story_handler(event, context):
-  if event["routeKey"] == "/api/v1.0/story":
-    dynamodb = boto3.resource("dynamodb", region_name=os["AWS_REGION"])
-    table = dynamodb.Table(os['STORIES_TABLE'])
-    response = table.scan(
-      IndexName="ListOfStories"
-    )
+class DecimalEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, decimal.Decimal):
+            return str(o)
+        if isinstance(o, set):
+            return list(o)
+        return super(DecimalEncoder, self).default(o)
 
-    return json.dumps(response['Items'])
+def story_handler(event, context):
+  if event["routeKey"] == "GET /api/v1.0/story":
+    table = dynamodb.Table(os.environ['STORIES_TABLE'])
+    dynamodb = boto3.resource("dynamodb")
+    results=[]
+    response = table.scan(
+        Limit=25,
+        IndexName="ListOfStories"
+        )
+
+    for i in response['Items']:
+        results.append(i)
+    
+    return json.dumps(results, indent=4, cls=DecimalEncoder)
 
   else:
-      return json.dumps('{"Error":"Unsupported Operation"}')
+      message = {"Error":"Unsupported Operation", "routeKey": event["routeKey"] }
+      return json.dumps(message)
