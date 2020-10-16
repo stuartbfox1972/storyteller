98e4235ffdbd2592bfb0f3240375c30a915d3bcb
Stuart Fox <stuart@stuartfox.ca>
2020-10-15 23:26:33 -0700

    Get xray working? v2

--- lambdas/build.sh
+++ lambdas/build.sh
@@ -8,6 +8,7 @@ cd lambdas
 for dir in $(find . -maxdepth 1 -type d | sed -e 's/\.\///g'|grep -v ^\\.)
 do
         cd "$dir";
+        ZIP="$dir"-"$LambdaVersion".zip
         if [ -f requirements.txt ];
         then
                 mkdir -p package
@@ -15,9 +16,9 @@ do
                 pip install --no-compile -t ./ -r ../requirements.txt
                 zip -r9 ../"$dir"-"$LambdaVersion".zip .
                 cd ..
-                zip -g "$dir"-"$LambdaVersion".zip "$dir".py
+                zip -g "$ZIP" "$dir".py
         else
-                zip -r "$dir"-"$LambdaVersion".zip ./
+                zip -r "$ZIP".zip ./
         fi
-        aws s3 cp "$dir"-"$LambdaVersion".zip s3://"${LambdaBucket}"/lambdas/"$dir"/"$dir"-"$LambdaVersion".zip
+        aws s3 cp "$dir"-"$LambdaVersion".zip s3://"${LambdaBucket}"/lambdas/"$dir"/"$ZIP"
 done
