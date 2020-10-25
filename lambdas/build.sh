125fbf83d28af3f8ae7b6aa801735a11c8f39c8d
Stuart Fox <stuart@stuartfox.ca>
2020-10-25 09:45:07 -0700

    Fix build.sh

--- lambdas/build.sh
+++ lambdas/build.sh
@@ -1,24 +1,25 @@
-#!/bin/bash 
+#!/bin/bash
 
 LambdaBucket=$1
 LambdaVersion=$2
 
 cd lambdas
+ROOT=$(pwd)
 
 for dir in $(find . -maxdepth 1 -type d | sed -e 's/\.\///g'|grep -v ^\\.)
 do
-        cd "$dir";
+        cd ${ROOT}/"$dir";
         ZIP="$dir"-"$LambdaVersion".zip
         if [ -f requirements.txt ];
         then
                 mkdir -p package
                 cd package
-                pip install --no-compile -t ./ -r ../requirements.txt
-                zip -r9 ../"$dir"-"$LambdaVersion".zip .
+                pip install -q --no-compile -t ./ -r ../requirements.txt
+                zip -q -r9 ../"$dir"-"$LambdaVersion".zip .
                 cd ..
-                zip -g "$ZIP" "$dir".py
+                zip -q -g "$ZIP" "$dir".py
         else
-                zip -r "$ZIP".zip ./
+                zip -q -r "$ZIP".zip ./
         fi
-        aws s3 cp "$dir"-"$LambdaVersion".zip s3://"${LambdaBucket}"/lambdas/"$dir"/"$ZIP"
+        echo aws s3 cp "$dir"-"$LambdaVersion".zip s3://"${LambdaBucket}"/lambdas/"$dir"/"$ZIP"
 done
