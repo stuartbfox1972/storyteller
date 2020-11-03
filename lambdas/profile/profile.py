03992bd53674a2f24de182d895835ddbceac1108
Stuart Fox <stuart@stuartfox.ca>
2020-11-02 16:40:05 -0800

    Flesh out getting a user profile

--- lambdas/profile/profile.py
+++ lambdas/profile/profile.py
@@ -5,20 +5,28 @@ import os
 import boto3
 
 from boto3.dynamodb.conditions import Key, Attr
-from aws_xray_sdk.core import xray_recorder
-from aws_xray_sdk.core import patch_all
+#from aws_xray_sdk.core import xray_recorder
+#from aws_xray_sdk.core import patch_all
+#patch_all()
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
-patch_all()
 
 def _connect():
-  """Connect to DynamoDB """
+  """ Connect to DynamoDB """
   dynamodb = boto3.resource("dynamodb")
   table = dynamodb.Table(os.environ['STORIES_TABLE'])
   return table
 
 def profile_handler(event, context):
-  """Entry point """
+  """ Entry point """
   table = _connect()
-  return "OK"
+  sub = event['requestContext']['authorizer']['jwt']['claims']['sub']
+  if event['routeKey'] == "GET /api/v1.0/profile":
+    result = table.get_item(Key={'PK': 'USER#' + sub,
+                                   'SK': "PROFILE"})
+    if 'Item' in result:
+      userdata = result['Item']
+      return json.dumps(userdata)
+
+    return '{"status":"user not found"}'
--- lambdas/story/story.py
+++ lambdas/story/story.py
@@ -6,12 +6,12 @@ import os
 import boto3
 
 from boto3.dynamodb.conditions import Key, Attr
-from aws_xray_sdk.core import xray_recorder
-from aws_xray_sdk.core import patch_all
+#from aws_xray_sdk.core import xray_recorder
+#from aws_xray_sdk.core import patch_all
+#patch_all()
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
-patch_all()
 
 class DecimalEncoder(json.JSONEncoder):
   """Some funky magic from StackOverflow"""
